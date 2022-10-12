af5254e1375b602e074c4ce9ad95c619396f5868
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-12 17:49:31 -0400

    Started navigation component

--- junglebeauty/src/App.js
+++ junglebeauty/src/App.js
@@ -4,13 +4,7 @@ const App = () => {
   return <div className="app">
     <body>
       <header>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
+        
       </header>
       <main>
 
---
+++ junglebeauty/src/components/navigation/navigation.component.jsx
@@ -0,0 +1,11 @@
+const Navigation = () => {
+    return (
+        <div className='navigation'>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
