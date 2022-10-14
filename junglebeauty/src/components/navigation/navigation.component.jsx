92d422b8e6c20a71e4e20cb072aeca3be7127293
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-13 23:20:08 -0400

    Implemented home page and navigation routes

--- junglebeauty/src/components/navigation/navigation.component.jsx
+++ junglebeauty/src/components/navigation/navigation.component.jsx
@@ -1,12 +1,29 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment, useContext } from "react";
+import JungleBeautyLogo from '../../assets/logo.png';
+import './navigation.styles.css';
+
 const Navigation = () => {
     return (
-        <div className='navigation'>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-link' to='/'>
+                    <img className='logo-img' src={JungleBeautyLogo}/>
+                </Link>
+                
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/'>Home</Link>
+                    <Link className='nav-link' to='/'>Queens</Link>
+                    <Link className='nav-link' to='/'>Kings</Link>
+                    <Link className='nav-link' to='/'>Kittens</Link>
+                    <Link className='nav-link' to='/'>News</Link>
+                    <Link className='nav-link' to='/'>FAQ</Link>
+                    <Link className='nav-link' to='/'>About</Link>
+                    <Link className='nav-link' to='/'>Contact</Link>
+                    <Link className='nav-link' to='/'>How to buy</Link>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
