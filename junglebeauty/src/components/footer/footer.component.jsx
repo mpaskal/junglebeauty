4b1a75f1b6ca86eceb1469937d61470a636fd53a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-18 03:51:35 -0400

    Added certification images to footer

--- junglebeauty/src/components/footer/footer.component.jsx
+++ junglebeauty/src/components/footer/footer.component.jsx
@@ -1,9 +1,16 @@
+import BccCert from '../../assets/certifications/BCC-Certified-Breeder.png';
+import RoyalCaninCert from '../../assets/certifications/royalcanin.png';
+import TicaCert from '../../assets/certifications/TICA-75-2.png';
 import './footer.styles.css';
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <h1>Certifications go here</h1>
+            <div className cert-container>
+                <img className='cert' src={TicaCert}/>
+                <img className='cert' src={BccCert}/>
+                <img className='cert' src={RoyalCaninCert}/>
+            </div>
         </div>
     )
 }
