0f3117f41b16b4c034f6699954fdb0ec64ae491d
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-19 01:02:17 -0400

    Got responsive navbar working

--- junglebeauty/src/components/navigation/navigation.component.jsx
+++ junglebeauty/src/components/navigation/navigation.component.jsx
@@ -1,37 +1,35 @@
+import React, { Fragment } from 'react';
 import { Link, NavLink } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import JungleBeautyLogo from '../../assets/logo.png';
 import './navigation.styles.css';
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect fixed='top' expand='xl'>
-            <Container fluid>
-                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                
-                    
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav>
-                            <Nav.Link href='/'>Home</Nav.Link>
-                            <Nav.Link href='/'>Queens</Nav.Link>
-                            <Nav.Link href='/'>Kings</Nav.Link>
-                            <Nav.Link href='/kittens'>Kittens</Nav.Link>
-                            <Nav.Link href='/'>News</Nav.Link>
-                            <Nav.Link href='/'>FAQ</Nav.Link>
-                            <Nav.Link href='/'>About</Nav.Link>
-                            <Nav.Link href='/'>Contact</Nav.Link>
-                            <Nav.Link href='/'>How to buy</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-            </Container>
-    </Navbar>
-      );
+            <Navbar collapseOnSelect className='nav-bar' expand="lg" variant='light'>
+                <Container>
+                    <Navbar.Brand className='logo-link' to='/'>
+                        <img className='logo-img' src={JungleBeautyLogo}/>
+                        <h1 className='logo-text'>JungleBeauty Bengals</h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav>
+                        <Nav.Link className='nav-link' to='/'>Home</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>Queens</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>Kings</Nav.Link>
+                        <Nav.Link className='nav-link' to='/kittens'>Kittens</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>News</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>FAQ</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>About</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>Contact</Nav.Link>
+                        <Nav.Link className='nav-link' to='/'>How to buy</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+       
+    );
 }
 
 export default Navigation;
