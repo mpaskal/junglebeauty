098feb3a063811bd9c3a31c1b6088ffa460cccd8
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-20 17:49:45 -0400

    Added social media icons to footer

--- junglebeauty/src/components/Footer.jsx
+++ junglebeauty/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import BccCert from '../assets/certifications/BCC-Certified-Breeder.png';
 import RoyalCaninCert from '../assets/certifications/royalcanin.png';
 import TicaCert from '../assets/certifications/TICA-75-2.png';
+import { SocialIcon } from 'react-social-icons';
 import './../App.css';
 
 const Footer = () => {
@@ -11,6 +12,12 @@ const Footer = () => {
                 <img className='cert' src={BccCert}/>
                 <img className='cert' src={RoyalCaninCert}/>
             </div>
+            <div className='social-container'>
+                <SocialIcon className='social-icon' url='https://www.instagram.com/junglebeautybengals/' fgColor='white'/>
+                <SocialIcon className='social-icon' url='https://www.facebook.com/junglebeautybengals' fgColor='white'/>
+                <SocialIcon className='social-icon' url='https://www.youtube.com/channel/UC5BbWtEY3t_uSvq6EZag9iA' fgColor='white'/>
+            </div>
+            <p>JungleBeautyBengals. Copyright © 2022</p>
         </div>
     )
 }
