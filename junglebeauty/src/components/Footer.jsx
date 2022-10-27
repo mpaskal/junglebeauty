3f272886151ee49a1f9a2cde2e78ec8d8e57ee21
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-27 02:19:10 -0400

    Converted footer to MDB

--- junglebeauty/src/components/Footer.jsx
+++ junglebeauty/src/components/Footer.jsx
@@ -1,21 +1,29 @@
-import { SocialIcon } from 'react-social-icons';
+import { MDBFooter, MDBContainer, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 import './../App.css';
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            <div className='cert-container'>
+        <MDBFooter className='footer'>
+            <MDBContainer className='cert-container'>
                 <img className='cert' src='/assets/certifications/TICA-75-2.png'/>
                 <img className='cert' src='/assets/certifications/BCC-Certified-Breeder.png'/>
                 <img className='cert' src='/assets/certifications/royalcanin.png'/>
-            </div>
-            <div className='social-container'>
-                <SocialIcon className='social-icon' url='https://www.instagram.com/junglebeautybengals/' fgColor='white'/>
-                <SocialIcon className='social-icon' url='https://www.facebook.com/junglebeautybengals' fgColor='white'/>
-                <SocialIcon className='social-icon' url='https://www.youtube.com/channel/UC5BbWtEY3t_uSvq6EZag9iA' fgColor='white'/>
-            </div>
-            <p>JungleBeautyBengals. Copyright © 2022</p>
-        </div>
+            </MDBContainer>
+            <MDBContainer className='social-container'>
+                <MDBBtn outline color='light' role='button' url='https://www.instagram.com/junglebeautybengals'>
+                    <MDBIcon fab icon='instagram'/>
+                </MDBBtn>
+                <MDBBtn outline color='light' role='button' url='https://www.facebook.com/junglebeautybengals'>
+                    <MDBIcon fab icon='facebook-f'/>
+                </MDBBtn>
+                <MDBBtn outline color='light' role='button' url='https://www.youtube.com/channel/UC5BbWtEY3t_uSvq6EZag9iA'>
+                    <MDBIcon fab icon='youtube'/>
+                </MDBBtn>
+            </MDBContainer>
+            <MDBContainer>
+                <p>JungleBeautyBengals. Copyright © 2022</p>
+            </MDBContainer>
+        </MDBFooter>
     )
 }
 
--- junglebeauty/src/index.js
+++ junglebeauty/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import "./index.css";
 import App from "./App";
 
