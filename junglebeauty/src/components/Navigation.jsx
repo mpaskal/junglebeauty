3585c41d9a6e50484572df6f28b1d67fe5a3b6c1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-10-26 23:59:41 -0400

    Improved navigation bar

--- junglebeauty/src/components/Navigation.jsx
+++ junglebeauty/src/components/Navigation.jsx
@@ -5,31 +5,31 @@ import './../App.css';
 
 const Navigation = () => {
     return (
-            <Navbar collapseOnSelect className='nav-bar' expand="lg" variant='light'>
+            <Navbar collapseOnSelect className='nav-bar' expand="lg" variant='dark'>
                 <Container>
                     <Navbar.Brand className='logo-link' to='/'>
                         <img className='logo-img' src='/assets/logo.png'/>
                         <h1 className='logo-text'>JungleBeauty Bengals</h1>
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
                     <Navbar.Collapse id='responsive-navbar-nav'>
-                    <Nav>
-                        <Link className='nav-link' to='/'>Home</Link>
-                        <Link className='nav-link' to='/queens'>Queens</Link>
-                        <Link className='nav-link' to='/kings'>Kings</Link>
-                        <Link className='nav-link' to='/kittens'>Kittens</Link>
-                        <Link className='nav-link' to='/news'>News</Link>
-                        <Link className='nav-link' to='/faq'>FAQ</Link>
-                        <Link className='nav-link' to='/about-us'>About</Link>
-                        <Link className='nav-link' to='/contact'>Contact</Link>
-                        <NavDropdown title='How to buy' id='responsive-navbar-dropdown'>
-                            <Link className='nav-link' to='/why-choose-junglebeauty'>Why choose JungleBeauty</Link>
-                            <Link className='nav-link' to='/buy-sell-agreement'>Buy-Sell Agreement</Link>
-                            <Link className='nav-link' to='/price'>Price</Link>
-                            <Link className='nav-link' to='/preparing-for-coming-kitten'>How to be ready for coming kitten</Link>
-                            <Link className='nav-link' to='/visit-junglebeauty'>How to visit JungleBeauty</Link>
-                        </NavDropdown>
-                    </Nav>
+                        <Nav className='nav-links-container'>
+                            <Link className='nav-link' to='/'>Home</Link>
+                            <Link className='nav-link' to='/queens'>Queens</Link>
+                            <Link className='nav-link' to='/kings'>Kings</Link>
+                            <Link className='nav-link' to='/kittens'>Available</Link>
+                            <Link className='nav-link' to='/news'>News</Link>
+                            <Link className='nav-link' to='/faq'>FAQ</Link>
+                            <Link className='nav-link' to='/about-us'>About</Link>
+                            <Link className='nav-link' to='/contact'>Contact</Link>
+                            <NavDropdown title='How to buy' id='responsive-navbar-dropdown'>
+                                <Link className='nav-link' to='/why-choose-junglebeauty'>Why choose JungleBeauty</Link>
+                                <Link className='nav-link' to='/buy-sell-agreement'>Buy-Sell Agreement</Link>
+                                <Link className='nav-link' to='/price'>Price</Link>
+                                <Link className='nav-link' to='/preparing-for-coming-kitten'>How to be ready for coming kitten</Link>
+                                <Link className='nav-link' to='/visit-junglebeauty'>How to visit JungleBeauty</Link>
+                            </NavDropdown>
+                        </Nav>
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
