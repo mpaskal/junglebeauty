35c78c3dbf542fb0bb76a802a7911ea8386f6d68
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-11-08 03:58:47 -0500

    Tweaked look of CatCardCarousel

--- junglebeauty/src/components/CatCardCarousel.jsx
+++ junglebeauty/src/components/CatCardCarousel.jsx
@@ -9,44 +9,46 @@ import './../App.css';
 
 const CatCardCarousel = ({ cats }) => {
   return (
-    <Carousel 
-      infinite
-      responsive={{
-        desktop: {
-          breakpoint: {
-            max: 3000,
-            min: 1024
+    <div className='card-container'>
+      <Carousel 
+        infinite
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024
+            },
+            items: 3,
           },
-          items: 3,
-        },
-        mobile: {
-          breakpoint: {
-            max: 464,
-            min: 0
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0
+            },
+            items: 1,
+            partialVisibilityGutter: 30
           },
-          items: 1,
-          partialVisibilityGutter: 30
-        },
-        tablet: {
-          breakpoint: {
-            max: 1024,
-            min: 464
-          },
-          items: 2,
-          partialVisibilityGutter: 30    
-        }
-      }}
-      rewind={false}
-      >
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464
+            },
+            items: 2,
+            partialVisibilityGutter: 30    
+          }
+        }}
+        rewind={false}
+        >
 
-      {cats.map((cat) => {
-        return (
-          <div key={cat.id}>
-            <CatCard cat={cat}/>
-          </div>
-        )
-      })}
-    </Carousel>
+        {cats.map((cat) => {
+          return (
+            <div key={cat.id}>
+              <CatCard cat={cat}/>
+            </div>
+          )
+        })}
+      </Carousel>
+    </div>
   );
 };
 
