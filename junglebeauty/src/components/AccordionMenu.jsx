8127c396d12fe79476b771fc00071e195d602a3d
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-11-18 21:27:16 -0500

    Worked on AccordionMenu component

--- junglebeauty/src/components/AccordionMenu.jsx
+++ junglebeauty/src/components/AccordionMenu.jsx
@@ -1,18 +1,21 @@
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Card, useAccordionButton } from 'react-bootstrap';
 import './../App.css';
 
 const AccordionMenu = ({ items }) => {
   var item_id = -1;
 
   return (
-    <Accordion className='accordion' defaultActiveKey='0'>
+    <Accordion className='accordion' defaultActiveKey='0' alwaysOpen flush>
       {items.map((item) => {
         item_id++;
         return (
-            <Accordion.Item eventKey={item_id} key={item_id}>
-                <Accordion.Header>{item.title}</Accordion.Header>
-                <Accordion.Body>{item.body}</Accordion.Body>
-            </Accordion.Item>
+            <Card key={item_id}>
+                <Card.Header className='accordion-header'>
+                  
+                  <p className='accordion-header-text'>{item.title}</p>
+                </Card.Header>
+                <Card.Body className='accordion-item'>{item.body}</Card.Body>
+            </Card>
         )
       })}
     </Accordion>
