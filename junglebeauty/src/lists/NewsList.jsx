ecfcbbb7c02976fb43c271cca9625615eb202b89
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-11-22 19:44:12 -0500

    Ported over some news items

--- junglebeauty/src/lists/NewsList.jsx
+++ junglebeauty/src/lists/NewsList.jsx
@@ -1,18 +1,147 @@
 const NewsList = [
     {
         title: 'Litter of Lolita',
-        body: `Four kittens are available for reservation.
-        
-        Release: ready to go from December 30. They are the best New Year gifts!
-
-        King: Julius, Queen: Lolita.
-        `,
+        body: 
+            <>
+                <p>
+                Four kittens are available for reservation.
+                </p><p>
+                Release: ready to go from December 30. They are the best New Year gifts!
+                </p><p>
+                King: Julius, Queen: Lolita.
+                </p>
+            </>,
         date: '2022-10-07'
     }, {
+        title: 'Litter of Bora',
+        body: 
+            <>
+                <p>
+                Four kittens are available for reservation
+                </p><p>
+                Release: ready to go from December 22. They are the best Christmas gifts!
+                </p><p>
+                King: Merlin, Queen: Bora.
+                </p>
+            </>,
+        date: '2022-09-29'
+    }, {
+        title: 'Litter of Elizabeth',
+        body: 
+            <>
+                <p>
+                Four kittens available for reservation.
+                </p><p>
+                Release: ready to go from December 21. They are the best Christmas gifts!
+                </p><p>
+                King: Churchil, Queen: Elizabeth.
+                </p>
+            </>,
+        date: '2022-09-28'
+    }, {
+        title: 'Litter of Eleanor',
+        body:
+            <>
+                <p>
+                King: Merlin, Queen: Eleanor
+                </p>
+            </>,
+        date: '2022-08-16'
+    }, {
+        title: 'Litter of Cleo',
+        body:
+            <>
+                <p>
+                One boy is available for reservation.
+                </p>
+            </>,
+        date: '2022-07-24'
+    }, {
         title: '',
-        body: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+            date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ''
+    }, {
+        title: '',
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
         date: ''
     }
+
+    /*
+    {
+        title: ``,
+        body:
+            <>
+                <p>
+
+                </p>
+            </>,
+        date: ``
+    }
+    */
 ];
 
 export default NewsList;
