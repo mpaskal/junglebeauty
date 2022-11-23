7e58f434597525aa99bc93c71547b0a779b9779c
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-11-22 19:41:16 -0500

    Tweaked AccordionMenu

--- junglebeauty/src/components/AccordionMenu.jsx
+++ junglebeauty/src/components/AccordionMenu.jsx
@@ -7,13 +7,14 @@ const AccordionMenu = ({ items }) => {
   var itemID = -1;
 
   return (
-    <Accordion className='accordion' defaultActiveKey='0' alwaysOpen flush>
+    <Accordion className='accordion' alwaysOpen flush>
       {items.map((item) => {
         itemID++;
+
         return (
             <Accordion.Item className='accordion-item' eventKey={itemID}>
                 <Accordion.Header className='accordion-header'>
-                  <p className='accordion-header-text'>{item.date ? ConvertDate(item.date) : ''}: {item.title}</p>
+                  <p className='accordion-header-text'>{item.date ? `${ConvertDate(item.date)}:` : ''} {item.title}</p>
                 </Accordion.Header>
                 <Accordion.Body>{item.body}</Accordion.Body>
             </Accordion.Item>
