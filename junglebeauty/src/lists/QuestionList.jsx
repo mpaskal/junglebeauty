9af1234928689eb1066e4eb6fe5fee19a031b993
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-11-22 19:44:24 -0500

    Ported over all question

--- junglebeauty/src/lists/QuestionList.jsx
+++ junglebeauty/src/lists/QuestionList.jsx
@@ -1,47 +1,118 @@
 const QuestionList = [
     {
         title: `Why can Bengals attack people?`,
-        body: `Bengals as any other cats (if they are healthy) attack people for one reason: they feel danger, they want to rescue themselves. The same way they attack dogs trying to scratch dogs eyes. You will need to understand what happened before and find the reason if your cat started attacking someone at your house. Most probably that person(s), often kids, forced or punished your cat before and the cat started thinking that it is better to attack first and avoid the same danger as he/she experienced before. It will take time for the cat to believe in that person again, and of course it will require some work.`
+        body:
+            <>
+                <p>
+                Bengals as any other cats (if they are healthy) attack people for one reason: they feel danger, they want to rescue themselves. The same way they attack dogs trying to scratch dogs eyes. You will need to understand what happened before and find the reason if your cat started attacking someone at your house. Most probably that person(s), often kids, forced or punished your cat before and the cat started thinking that it is better to attack first and avoid the same danger as he/she experienced before. It will take time for the cat to believe in that person again, and of course it will require some work.
+                </p>
+            </>
     }, {
         title: `Bengal Cats: punishment or training?`,
-        body: `Punishment can destroy the relationship between you and your cat. What is punishment? -
-
-        If people do something not pleasant or causes pain for cat. In such situation every cat can start thinking that human is attacking him/her. She/he may not connect panishment to something she/he has done. Cats always think they have done everything correctly.
-        
-        The best time to show to cat that something is done not the right way is to explain the proper behaviour right away after or during the wrong actions of the cat. Stop the wrong action, offer an alternative, prevent the wrong action from happening/demotivate your cat to do so, and give a treat/ please your cat for the correct actions. Training is always better than punishment. That is why if you want to stop some unwanted actions it is better to do it in such a manner that the cat does not see the owner is stopping him. For example, if you use a water gun to stop the cat, do it when your cat does not see the water gun in your hands.
-        
-        Bengal cats are highly trainable, they love learning! Always show the correct way by offering an alternative to unwanted behaviour.
-        
-        Remember that a lot of actions the intact cat does are caused by hormones and it is hardly possible to stop them without desexing. Spay/neuter your cat to avoid health and behavioural problems: allow him/he to be a perfect and happy pet!
-        `
+        body:
+            <>
+                <p>
+                Punishment can destroy the relationship between you and your cat. What is punishment? -
+                </p><p>
+                If people do something not pleasant or causes pain for cat. In such situation every cat can start thinking that human is attacking him/her. She/he may not connect panishment to something she/he has done. Cats always think they have done everything correctly.
+                </p><p>
+                The best time to show to cat that something is done not the right way is to explain the proper behaviour right away after or during the wrong actions of the cat. Stop the wrong action, offer an alternative, prevent the wrong action from happening/demotivate your cat to do so, and give a treat/ please your cat for the correct actions. Training is always better than punishment. That is why if you want to stop some unwanted actions it is better to do it in such a manner that the cat does not see the owner is stopping him. For example, if you use a water gun to stop the cat, do it when your cat does not see the water gun in your hands.
+                </p><p>
+                Bengal cats are highly trainable, they love learning! Always show the correct way by offering an alternative to unwanted behaviour.
+                </p><p>
+                Remember that a lot of actions the intact cat does are caused by hormones and it is hardly possible to stop them without desexing. Spay/neuter your cat to avoid health and behavioural problems: allow him/he to be a perfect and happy pet!
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `Bengal Cats: boys vs. girls`,
+        body:
+            <>
+                <p>
+                In Bengal kitten boys are more curious and vocal, also more crazy in actions and more interactive with people, they usually follow owners in everythind they do. Bengal girls are more careful and considerate, also usually more bossy, they can do some crazy things but it is not very common. You know that Bengals are very active cats if compare with other breeds. Of course there are some exceptions as everywhere, and some girls can be like boys or some boys can be like girls. You can find some more information online, we just share Junglebeauty Cattery experience.
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `Can Bengals do any damage in the house? What kind of damage?`,
+        body: 
+            <>
+                <p>
+                Yes, as every cat they can do some damage, mostly scratching. Every cat has natural right to be a cat. As Bengals are highly active and curious they can destroy some items as well. Basically you have to choose to have a cat or to have perfect and completely ordered house. And Bengals are not an exception. If you are not ready for that please do not consider to have a cat.
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `Can Bengals cause an allergy?`,
+        body: 
+            <>
+                <p>
+                Yes, they can. However, they are reported as <a href='https://www.petmd.com/cat/slideshows/care/10-hypoallergenic-cats'>hypoallergenic</a> as they rarely cause allergies.
+                </p><p>
+                    <a href='https://www.purina.co.uk/cats/behaviour-and-training/understanding-cat-behaviour/hypoallergenic-cat-breeds#:~:text=These%20cats%20are%20said%20to,around%20and%20playing%20with%20water'>
+                    “These cats are said to produce less Fel d 1 protein than many other breeds and shed less hair, and owners with allergies report that Bengals rarely trigger them.”
+                    </a>
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `What age will kitten be available to go home?`,
+        body:
+            <>
+                <p>
+                We release kittens when we see that he\she is ready to go, usually it occurs at the age of 12-16 weeks. However if kitten has some temporary disorder or too attached to the mom we may keep the kitten longer. We never put our kittens at risk and release them sick.
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `Can Bengals improve your mood or give positive emotions?`,
+        body:
+        <>
+            <p>
+            Bengal cat is a good source of therapy from the early age. However it is very important to chose the people oriented and friendly kitten. Bengals can be the source of a lot of positive emotions as you can enjoy simply by their appearance, their silky coat, golden or silver glitter, gorgeous movement and of course their intelligence and jumps.
+            </p>
+        </>
     }, {
-        title: ``,
-        body: ``
+        title: `How the deposit works`,
+        body: [<p>oy</p>],
     }, {
-        title: ``,
-        body: ``
+        title: `Tips to contact us and choose a kitten`,
+        body:
+            <>
+                <p>
+                Deposit has two purposes - to reserve the kitten and to be used for neutering. Our Junglebeauty Bengals Cattery takes the deposit CAD 400. We do not support backdoor breeding and try to eliminate it the best way we can. If we will not be able to do neutering with our clinic then you have to do it with your vet and deposit will be returned to you in full upon the proof of neutering/ spaying.
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `How the waiting list works`,
+        body:
+            <>
+                <p>
+                Please tell us about yourself, without this information we can not see you as a serious buyer. Tell us if you are interested in male or female specifically. You are welcome to ask questions, but we expect you to make your research about Bengals first. We are happy to share our experience of having Bengals as not every information on Internet is correct.
+                </p>
+            </>
     }, {
-        title: ``,
-        body: ``
+        title: `Procedure to reserve a kitten`,
+        body:
+            <>
+                <p>
+                We may ask you for CAD 100 to put you in the waiting list which will be counted towards reservation fee in the future.
+                </p><p>
+                You can ask us to put you in the waiting list if no kittens are available or you are waiting for the kitten from the specific queen \ king.
+                </p><p>
+                We will notify people from the waiting list before advertising. So you will have a priority to choose if you will be interested at that time. If you did not pay to be in the waiting list, we will wait 24 hours for your reply after notification, then discard your name from the list if we do not get any reply from you. Please tell us how would you like to be notified - email or text message over the phone. If your name is discarded from the waiting list you will not have a priority to choose a kitten however you are welcome to contact us for the available kittens.
+                </p><p>
+                In case you are registered for the waiting list with CAD100, it will be subtracted from reservation fee which is normally CAD 300 for the pet. We will wait for your reply within 3 days and you will always have a priority to choose from available kittens. However if you do not reply within 3 days the first priority to pick the kitten maybe given to the next interested person.
+                </p>
+            </>
     }
+
+    /*
+    {
+        title: ``,
+        body:
+            <>
+                <p>
+
+                </p>
+            </>
+    }
+    */
 ];
 
 export default QuestionList;
