212be390cab52461db5115f6afab9f35d6956275
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-16 23:01:56 -0500

    Removed unnecessary import

--- junglebeauty/src/components/DBFunctions.jsx
+++ junglebeauty/src/components/DBFunctions.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { collection, doc, query, where, getDocs, addDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { ref, getStorage, getDownloadURL } from 'firebase/storage';
 import { db } from '../firebase';
-import { GetCatFilepath } from './Functions';
 
 export async function QueryCats(table, predicate = []) {
     const cats = [];
@@ -65,9 +64,8 @@ export async function GetImage(filepath) {
     return url;
 }
 
-export function GetAllImages(cat) {
+export function GetAllImages(filepath) {
     const storage = getStorage();
-    const filepath = GetCatFilepath(cat);
     const images = [];
     
     
