886de442dcf7b1a56ec6b6b203e107c04f90e18d
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-18 23:33:15 -0500

    Changed ImageCarousel to take cat instead of images as prop

--- junglebeauty/src/components/ImageCarousel.jsx
+++ junglebeauty/src/components/ImageCarousel.jsx
@@ -3,10 +3,18 @@ import { Modal, Button } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
 import CatProfile from './KittenProfile';
 import CatCard from './CatCard';
+import CatImage from './CatImage';
 import { GetCatFilepath } from './Functions';
 import './../App.css';
 
-const ImageCarousel = ({ images }) => {  
+const ImageCarousel = ({ cat }) => {  
+    const [images, setImages] = useState([]);
+
+    
+
+    console.log('carousel');
+    console.log(images);
+
   return (
     <Carousel 
         infinite
@@ -40,7 +48,7 @@ const ImageCarousel = ({ images }) => {
     >
         {images.map((image) => {
             return (
-                <img className='cat-img' key={image} src={image}/>
+                <CatImage filepath={image} />
             )
         })}
     </Carousel>
