4e7cc7b7c2a20f01ca64dae4562b73d48f0d5b99
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-19 23:13:18 -0500

    Fixed error in call to GetAllImages()

--- junglebeauty/src/components/ImageCarousel.jsx
+++ junglebeauty/src/components/ImageCarousel.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import CatImage from './CatImage';
+import { GetCatFilepath } from './Functions';
 import { GetAllImages } from './DBFunctions';
 import './../App.css';
 
@@ -8,7 +9,7 @@ const ImageCarousel = ({ cat }) => {
     const [images, setImages] = useState([]);
 
     const getData = async () => {
-        const images = GetAllImages(cat);
+        const images = await GetAllImages(GetCatFilepath(cat));
         setImages(images);
       }
     
@@ -58,5 +59,44 @@ const ImageCarousel = ({ cat }) => {
     </Carousel>
   );
 };
+
+/*
+<Carousel 
+        infinite
+        responsive={{
+            desktop: {
+            breakpoint: {
+                max: 3000,
+                min: 1024
+            },
+            items: 1,
+            },
+            mobile: {
+            breakpoint: {
+                max: 464,
+                min: 0
+            },
+            items: 1,
+            partialVisibilityGutter: 30
+            },
+            tablet: {
+            breakpoint: {
+                max: 1024,
+                min: 464
+            },
+            items: 1,
+            partialVisibilityGutter: 30    
+            }
+        }}
+        rewind={false}
+        showDots
+    >
+        {images.map((image) => {
+            return (
+                <CatImage filepath={image} />
+            )
+        })}
+    </Carousel>
+*/
 
 export default ImageCarousel;
