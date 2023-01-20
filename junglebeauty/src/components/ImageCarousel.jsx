011236ce10d3ecb83a43308b2e80697733580426
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-01-20 00:16:05 -0500

    Removed console messages

--- junglebeauty/src/components/ImageCarousel.jsx
+++ junglebeauty/src/components/ImageCarousel.jsx
@@ -7,7 +7,7 @@ import './../App.css';
 
 const ImageCarousel = ({ cat }) => {  
     const [images, setImages] = useState([]);
-
+  
     const getData = async () => {
         const images = await GetAllImages(GetCatFilepath(cat));
         setImages(images);
@@ -17,10 +17,7 @@ const ImageCarousel = ({ cat }) => {
         getData();
     }, [])
 
-    console.log('carousel');
-    console.log(images);
-
-  return (
+    return (
     <Carousel 
         infinite
         responsive={{
@@ -59,44 +56,5 @@ const ImageCarousel = ({ cat }) => {
     </Carousel>
   );
 };
-
-/*
-<Carousel 
-        infinite
-        responsive={{
-            desktop: {
-            breakpoint: {
-                max: 3000,
-                min: 1024
-            },
-            items: 1,
-            },
-            mobile: {
-            breakpoint: {
-                max: 464,
-                min: 0
-            },
-            items: 1,
-            partialVisibilityGutter: 30
-            },
-            tablet: {
-            breakpoint: {
-                max: 1024,
-                min: 464
-            },
-            items: 1,
-            partialVisibilityGutter: 30    
-            }
-        }}
-        rewind={false}
-        showDots
-    >
-        {images.map((image) => {
-            return (
-                <CatImage filepath={image} />
-            )
-        })}
-    </Carousel>
-*/
 
 export default ImageCarousel;
