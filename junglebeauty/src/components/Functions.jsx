be443e2baec4f08bf84f8eeb3a79efd9221e411e
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-08 05:17:23 -0500

    Changed GetCatFilepath() to use cat ID rather than name and date fields to account for updated cats

--- junglebeauty/src/components/Functions.jsx
+++ junglebeauty/src/components/Functions.jsx
@@ -7,7 +7,10 @@ export function GetCatID(cat) {
   }
 
 export function GetCatFilepath(cat) {
-  const { name, sex, mother, date } = cat;
+  const { id, sex, mother } = cat;
+  const idValues = id.split('.');
+  const name = idValues[0];
+  const date = idValues[1];
   const type = (mother ? `Kitten` : `${sex == 'male' ? `King` : `Queen`}`);
   const filepath = `${type}s/${type == 'Kitten' ? `${date}/` : ``}${name}/`;
 
