3e03ff756ca9836460ac55bae0176ed62c26ba35
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-23 23:51:14 -0400

    Created VideoCarousel component

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
+import VideoFrame from './VideoFrame';
+import { GetCatFilepath } from './Functions';
+import { GetAllImages } from './FirebaseFunctions';
+import './../App.css';
+
+const VideoCarousel = ({ videos = [] }) => {  
+    return (
+        <Carousel 
+            infinite
+            responsive={{
+                desktop: {
+                breakpoint: {
+                    max: 3000,
+                    min: 1024
+                },
+                items: 1,
+                },
+                mobile: {
+                breakpoint: {
+                    max: 464,
+                    min: 0
+                },
+                items: 1,
+                partialVisibilityGutter: 30
+                },
+                tablet: {
+                breakpoint: {
+                    max: 1024,
+                    min: 464
+                },
+                items: 1,
+                partialVisibilityGutter: 30    
+                }
+            }}
+            rewind={false}
+            showDots
+        >
+            {videos.map((video) => {
+                return (
+                    <VideoFrame key={video} id={video} />
+                )
+            })}
+        </Carousel>
+  );
+};
+
+export default VideoCarousel;
