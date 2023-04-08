ac3e3b26e287045ddcb325250ca53ba8f78cb0fc
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-07 22:09:14 -0400

    Created CatsContext

---
+++ src/contexts/CatsContext.jsx
@@ -0,0 +1,29 @@
+import React, { useContext, useState, useEffect } from "react";
+import { QueryCats } from "../components/FirebaseFunctions";
+
+const CatsContext = React.createContext();
+
+export function CatsProvider({ children }) {
+    const [cats, setCats] = useState(null);
+
+    const getCats = async () => {
+        const parents = await QueryCats('parents');
+        const kittens = await QueryCats('kittens');
+        
+        setCats({parents, kittens})
+    }
+    
+    useEffect(() => {
+        getCats();
+    }, []);
+
+    return (
+        <CatsContext.Provider value={cats}>
+            {children}
+        </CatsContext.Provider>
+    );
+}
+
+export function useCats() {
+    return useContext(CatsContext);
+}
