fefd61e1a4f1a93c11f2647cc90e74c3805a5b2a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-17 23:56:49 -0400

    Worked on appearance of kitten profile

--- src/components/KittenProfile.jsx
+++ src/components/KittenProfile.jsx
@@ -27,7 +27,8 @@ const KittenProfile= ({ cat = [] }) => {
     return (
         <div className='kitten-columns-container'>
             <div>
-                <h3>{name} collar {sex == 'male' ? 'boy' : 'girl'}. Born {birthDate}.</h3>
+                <h2>{name} collar {sex == 'male' ? 'boy' : 'girl'}</h2>
+                <h4>Born {birthDate}</h4>
                 <p>Mother: <Link to='/queens' state={mother}>{mother}</Link>, Father: <Link to='/kings' state={father}>{father}</Link></p>
                 <p>Date of release: {releaseDate <= currentDate ? 'ready to go!' : ConvertDate(releaseDate)}</p>
                 {price ? <p>Price: ${price}</p> : ''}
