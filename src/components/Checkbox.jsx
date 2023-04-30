59474724905cb116898ef3a1cd16e28d85e12692
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-29 22:31:55 -0400

    Worked on checkbox component

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
-const Checkbox = ( defaultCheck = false ) => {
-    const [checkec, setChecked] = useState(defaultCheck);
+const Checkbox = ( label, name, value, defaultCheck = false ) => {
+    const [checked, setChecked] = useState(defaultCheck);
 
-   
+    const handleChange = () => {
+        setChecked(!checked);
+    }
+
+    return () {
+        <label>
+            <p>{label}</p>
+            <input type='checkbox' name={name} value={value} checked={defaultCheck} onChange={handleChange} />
+        </label>
+    }
 }
 
 export default Checkbox;
