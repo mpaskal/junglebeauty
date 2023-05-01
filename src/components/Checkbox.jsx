d7cfddbc85c481b7017fb49e98420d26f08df39d
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-04-30 20:32:50 -0400

    Added change handler prop

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-const Checkbox = ({ label, name, value, defaultCheck = false }) => {
+const Checkbox = ({ label, name, value, changeHandler, defaultCheck = false }) => {
     const [checked, setChecked] = useState(defaultCheck);
 
     const handleChange = () => {
         setChecked(!checked);
+        changeHandler();
     }
 
     return (
