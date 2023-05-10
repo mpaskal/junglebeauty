eef1de4e00e12e7a7a3bed73d2cc240b891ebd47
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-09 22:12:08 -0400

    Added certification links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,18 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className='cert-container'>
-                <img className='cert' src='/assets/certifications/TICA-75-2.png' />
-                <img className='cert' src='/assets/certifications/CCA_logo_h75.png' />
-                <img className='cert' src='/assets/certifications/BCC-Certified-Breeder.png' />
-                <img className='cert' src='/assets/certifications/royalcanin.png' />
+                <a href='https://tica.org/bengal-breeders?view=article&id=2253:cattery-junglebeauty&catid=97' target='_blank' rel="noopener noreferrer">
+                    <img className='cert' src='/assets/certifications/TICA-75-2.png' />
+                </a>
+                <a href='https://www.cca-afc.com/Breeds/Breeders' target='_blank' rel="noopener noreferrer">
+                  <img className='cert' src='/assets/certifications/CCA_logo_h75.png' />
+                </a>
+                <a href='https://bengalcatclub.com/bengal-cat-breeders/canada-ontario/' target='_blank' rel="noopener noreferrer">
+                    <img className='cert' src='/assets/certifications/BCC-Certified-Breeder.png' />
+                </a>
+                <a href='https://royalcanin.com/' target='_blank' rel="noopener noreferrer">
+                    <img className='cert' src='/assets/certifications/royalcanin.png' />
+                </a>
             </div>
             <div className='social-container'>
                 <a className='social-icon' href='https://www.instagram.com/junglebeautybengals' target='_blank' rel="noopener noreferrer">
