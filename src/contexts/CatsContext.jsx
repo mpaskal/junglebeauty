70233e7ff6cc4d361263775fb48aa1fecf40fc84
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-13 19:41:10 -0400

    Added loading state

--- src/contexts/CatsContext.jsx
+++ src/contexts/CatsContext.jsx
@@ -4,24 +4,24 @@ import { QueryCats } from "../components/FirebaseFunctions";
 const CatsContext = React.createContext();
 
 export function CatsProvider({ children }) {
-    const [cats, setCats] = useState(null);
+    const [cats, setCats] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getCats = async () => {
         const parents = await QueryCats('parents');
         const kittens = await QueryCats('kittens');
         
-        setCats({parents, kittens})
+        setCats({parents, kittens});
+        setLoading(false);
     }
     
     useEffect(() => {
         getCats();
     }, []);
 
-    console.log(cats);
-
     return (
         <CatsContext.Provider value={cats}>
-            {children}
+            { !loading && children }
         </CatsContext.Provider>
     );
 }
