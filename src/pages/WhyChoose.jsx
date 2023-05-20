3482949299d5b855ad2b976d1efe85d1ed527e83
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-19 22:04:40 -0400

    Converted selling points list into accordion

--- src/pages/WhyChoose.jsx
+++ src/pages/WhyChoose.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Accordion } from 'react-bootstrap';
 import './../App.css';
 
 const WhyChoose = () => {
@@ -10,42 +11,79 @@ const WhyChoose = () => {
                 <h1>Choice making - why JungleBeauty Bengal Cattery?</h1>
                 <h3>There are many good reasons!</h3>
                 <br/>
-                <h3>Choice</h3>
-                <p>
-                  You can choose a kitten that was born for you among other beautiful Bengal kittens.
-                </p><p>
-                  Due to the regular litters from our various queens, you have a chance to see different patterns and characteristics and choose the kitten that resonates with you most, whether it is a pattern on their back, the glow in their eyes or the spring in their step!
-                </p>
-                <h3>Predictability</h3>
-                <p>
-                  You will get a kitten that exemplifies what you want in a companion.
-                </p><p>
-                  You can see the kitten's parents and siblings.
-                </p><p>
-                  You will know what to expect in the future after 1-2 years, like size, patterns, appearance, temperament, health, etc.
-                </p>
-                <h3>Health</h3>
-                <p>
-                  Buying a kitten from Junglebeauty Cattery means you know the parents' history, their health and genes, and their temperament.
-                </p><p>
-                  You will get a <Link className='text-link' to='/price'>health warranty</Link> for the Kitten.
-                </p><p>
-                  Your furriend will not die suddenly from the congenital disorder at the age of 2-3 years.
-                </p>
-                <h3>Desexing</h3>
-                <p>
-                  All our kittens come to the forever home after spaying/neutering procedures.
-                </p><p>
-                  We do it at respectful vet clinics at the when the kitten's age is 11-14 weeks old. It is a very safe and painless surgery.
-                </p><p>
-                  Then our kittens grow without any sexual behaviour or unpleasant actions like yelling, making markers, and illnesses specific to Bengal female cats like pyometra.
-                </p>
-                <h3>Our support</h3>
-                <p>
-                  We stay in touch with our customers after they take home a kitten and are ready to answer any questions.
-                </p><p>
-                  We know the breed and want only the best for Kitten and you.
-                </p>
+                <Accordion className='accordion' alwaysOpen flush>
+                  <Accordion.Item className='accordion-item' eventKey={0}>
+                    <Accordion.Header className='accordion-header'>
+                      <h3 className='accordion-header-text'>Choice</h3>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <p>
+                        You can choose a kitten that was born for you among other beautiful Bengal kittens.
+                      </p><p>
+                        Due to the regular litters from our various queens, you have a chance to see different patterns and characteristics and choose the kitten that resonates with you most, whether it is a pattern on their back, the glow in their eyes or the spring in their step!
+                      </p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item className='accordion-item' eventKey={1}>
+                    <Accordion.Header className='accordion-header'>
+                      <h3 className='accordion-header-text'>Predictability</h3>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <p>
+                        You will get a kitten that exemplifies what you want in a companion.
+                      </p><p>
+                        You can see the kitten's parents and siblings.
+                      </p><p>
+                        You will know what to expect in the future after 1-2 years, like size, patterns, appearance, temperament, health, etc.
+                      </p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item className='accordion-item' eventKey={2}>
+                    <Accordion.Header className='accordion-header'>
+                      <h3 className='accordion-header-text'>Health</h3>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <p>
+                        Buying a kitten from Junglebeauty Cattery means you know the parents' history, their health and genes, and their temperament.
+                      </p><p>
+                        You will get a <Link className='text-link' to='/price'>health warranty</Link> for the Kitten.
+                      </p><p>
+                        Your furriend will not die suddenly from the congenital disorder at the age of 2-3 years.
+                      </p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item className='accordion-item' eventKey={3}>
+                    <Accordion.Header className='accordion-header'>
+                      <h3 className='accordion-header-text'>Desexing</h3>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <p>
+                        All our kittens come to the forever home after spaying/neutering procedures.
+                      </p><p>
+                        We do it at respectful vet clinics at the when the kitten's age is 11-14 weeks old. It is a very safe and painless surgery.
+                      </p><p>
+                        Then our kittens grow without any sexual behaviour or unpleasant actions like yelling, making markers, and illnesses specific to Bengal female cats like pyometra.
+                      </p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item className='accordion-item' eventKey={4}>
+                    <Accordion.Header className='accordion-header'>
+                      <h3 className='accordion-header-text'>Our support</h3>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <p>
+                        We stay in touch with our customers after they take home a kitten and are ready to answer any questions.
+                      </p><p>
+                        We know the breed and want only the best for Kitten and you.
+                      </p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+                
               </Col>
               <Col sm={3}>
                 <Link className='text-link' to='/kittens'>
