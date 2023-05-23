2f7ae362b90a8d285afdff94e067a61e13e76382
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-22 22:20:38 -0400

    Renamed 'category' prop back to 'name'

--- src/components/KittenGallery.jsx
+++ src/components/KittenGallery.jsx
@@ -10,7 +10,6 @@ const KittenGallery = () => {
   var kittens = [];
   var parents = [];
   const [filters, setFilters] = useState({colour: [], father: [], mother: [], status: []});
-  const [allCheck, setAllCheck] = useState();
   
   if (cats) {
       kittens = cats.kittens;
@@ -22,21 +21,22 @@ const KittenGallery = () => {
   }, [cats])
 
   const handleChange = (event) => {
-    const { category, value } = event.target;
-    var filterArray = filters[category];
+    const { name, value } = event.target;
+    var filterArray = filters[name];
+    
     if (filterArray.includes(value)) {
       filterArray = filterArray.filter(item => item != value);
     } else {
       filterArray.push(value);
     }
-    setFilters({ ...filters, [category]: filterArray});
+    setFilters({ ...filters, [name]: filterArray});
   }
 
   const toggleAll = () => {
-    if (!allCheck) {
-      setAllCheck(true);
+    if (filters['colour'].length > 0 || filters['father'].length > 0 || filters['mother'].length > 0 || filters['status'].length > 0) {
+      setFilters({colour: [], father: [], mother: [], status: []});
     } else {
-      setAllCheck(false);
+      setFilters({colour: ['silver', 'brown'], father: parents.filter((cat) => cat.sex === 'male').map((cat) => {return cat.name}), mother: parents.filter((cat) => cat.sex === 'female').map((cat) => {return cat.name}), status: ['available']});
     }
   }
 
@@ -52,12 +52,12 @@ const KittenGallery = () => {
               </p>
             </Accordion.Header>
             <Accordion.Body>
-              <Checkbox label='Available kittens' category='status' value='available' handleChange={handleChange} checked={filters['status'].includes('available')} />
-              <Checkbox label='Reserved kittens' category='status' value='reserved' handleChange={handleChange} checked={filters['status'].includes('reserved')} />
-              <Checkbox label='Graduated kittens' category='status' value='graduated' handleChange={handleChange} checked={filters['status'].includes('graduated')} />
+              <Checkbox label='Available kittens' name='status' value='available' handleChange={handleChange} checked={filters['status'].includes('available')} />
+              <Checkbox label='Reserved kittens' name='status' value='reserved' handleChange={handleChange} checked={filters['status'].includes('reserved')} />
+              <Checkbox label='Graduated kittens' name='status' value='graduated' handleChange={handleChange} checked={filters['status'].includes('graduated')} />
               <br/>
-              <Checkbox label='Silver' category='colour' value='silver' handleChange={handleChange} checked={filters['colour'].includes('silver')} />
-              <Checkbox label='Brown' category='colour' value='brown' handleChange={handleChange} checked={filters['colour'].includes('brown')} />
+              <Checkbox label='Silver' name='colour' value='silver' handleChange={handleChange} checked={filters['colour'].includes('silver')} />
+              <Checkbox label='Brown' name='colour' value='brown' handleChange={handleChange} checked={filters['colour'].includes('brown')} />
               <br/>
   
               <div className='parent-accordions-container'>
@@ -69,7 +69,7 @@ const KittenGallery = () => {
                     <Accordion.Body>
                       {parents.filter((cat) => cat.sex === 'male').map((cat) => {
                         return (
-                          <Checkbox key={cat.id} label={cat.name} category='father' value={cat.name} handleChange={handleChange} checked={filters['father'].includes(cat.name)} />
+                          <Checkbox key={cat.id} label={cat.name} name='father' value={cat.name} handleChange={handleChange} checked={filters['father'].includes(cat.name)} />
                         )
                       })}
                     </Accordion.Body>
@@ -83,7 +83,7 @@ const KittenGallery = () => {
                     <Accordion.Body>
                       {parents.filter((cat) => cat.sex === 'female').map((cat) => {
                         return (
-                          <Checkbox key={cat.id} label={cat.name} category='mother' value={cat.name} handleChange={handleChange} checked={filters['mother'].includes(cat.name)} />
+                          <Checkbox key={cat.id} label={cat.name} name='mother' value={cat.name} handleChange={handleChange} checked={filters['mother'].includes(cat.name)} />
                         )
                       })}
                     </Accordion.Body>
