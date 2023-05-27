7adffa0e9c1216b4a76bbd36dea575547e3a3375
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-26 20:19:00 -0400

    Edited text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,19 @@ const About = () => {
         <h1>JungleBeauty Bengals: vivid piece of jungle beauty in your home</h1>
         <br/>
         <p>
-          Junglebeauty is a TICA REGISTERED in-home cattery located in the KW area, Ontario, Canada. We focus on high quality and wide range of brown spotted Bengals with three distinct colours and on exceptional silver spotted Bengals. Two colours rosettes of small and medium size give the cat a wild-like look resembling wild spotted cats: jaguar, leopards, snow and clouded leopards.
+          Junglebeauty is a TICA REGISTERED in-home cattery located in the KW area, Ontario, Canada. We focus on high quality brown spotted Bengals 
+          with three distinct colours and on exceptional silver spotted Bengals. Two colours of small and medium sized rosettes give the cat a wild-like 
+          look resembling wild spotted cats: jaguars, snow leopards and clouded leopards.
         </p><p>
-          Currently we have queens from light brown and golden to charcoal colour that were born in Ontario or Quebec, Canada. We also have exceptionally high quality silver Queens. Our king Churchil is TICA champion bloodline Bengal cat stud.
+          Currently we have queens from light brown and golden to charcoal colour that were born in Ontario or Quebec, Canada. We also have 
+          exceptionally high quality silver Queens. Our king Churchil is a TICA champion bloodline Bengal cat stud.
         </p><p>
-          Kittens are raised indoors with love and high attention from the first days of their life.
+          Kittens are raised indoors with love and attention from the first days of their life.
         </p><p>
-          The health of our cats and kittens is our first priority, so we do our best to support and develop their strong immune systems. Kittens are socialized living and interacting with people and other pets (cats, parrot and dog) in a non-caged environment. All of them are highly people-oriented and affectionate. Kittens are usually released at the age of 11-14 weeks when they feel comfortable to live without their mom and are ready to explore the world. They exercise a lot playing on cat trees and cat pipes, jumping, running and climbing.
+          The health of our cats and kittens is our top priority, so we do our best to support and develop their strong immune systems. Kittens 
+          are socialized living and interacting with people and other pets (cats, parrot and dog) in a non-caged environment. All of them are 
+          highly people-oriented and affectionate. Kittens are usually released at the age of 11-14 weeks when they feel comfortable to live 
+          without their mom and are ready to explore the world. They exercise a lot playing on cat trees and cat pipes, jumping, running and climbing.
         </p><p>
           We also have a cat exercise wheel for adult cats to run and walk.
         </p>
