c3e73daed06a0bc03379095d70ece483a33aaaea
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-05-26 20:19:21 -0400

    Edited text

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -4,8 +4,8 @@ const Price = () => {
     return (
       <div>
         <div className='page-background'>
-          <h2>Price for kitten is 1200 - 2600 CAD.</h2>
-          <h2>What price includes:</h2>
+          <h2>The price for a kitten is 1200 - 2600 CAD.</h2>
+          <h2>What the price includes:</h2>
           <ul className='list-container'>
             <li>Two sets of vaccines</li>
             <li>Spay/neuter surgery</li>
@@ -33,11 +33,11 @@ const Price = () => {
           </p><p>
             It will be the responsibility of the Purchaser to have the Kitten examined by a veterinarian within 72 hours.
           </p><p>
-            Within that 72 hours period, if found to be seriously medically deficient, the Seller will take the Kitten back and refund payment, less the deposit, under the following conditions: the Seller is provided with proper documentation, from a licensed veterinarian, stating the medical issue and that the Kitten is seriously medically deficient; a second examination by a veterinarian of the Seller’s choice confirms these claims.
+            Within that 72 hours period, if found to be seriously ill, the Seller will take the Kitten back and refund payment, minus the deposit, under the following conditions: the Seller is provided with proper documentation, from a licensed veterinarian, stating the medical issue and that the Kitten is seriously medically deficient; a second examination by a veterinarian of the Seller’s choice confirms these claims.
           </p><p>
-            This Kitten is not guaranteed against respiratory disease, fleas, ringworms or any nuisance disease, environmental stress (such as diarrhea etc.) and disease caused by dietary changes.
+            This Kitten is not guaranteed against respiratory disease, fleas, ringworms or any parasitic disease, environmental stress (such as diarrhea etc.) and disease caused by dietary changes.
           </p><p>
-            After the 72 hours period, the Seller can no longer guarantee the Kitten’s health, except for birth defects.
+            After the 72 hour period, the Seller can no longer guarantee the Kitten’s health, except for birth defects.
           </p>
         </div>
         <div className='page-background'>
@@ -45,7 +45,7 @@ const Price = () => {
           <p>
             The Seller guarantees the Kitten against lethal genetic or congenital defects for 24 months following the date of purchase.
           </p><p>
-          If, during these 24 months, the Kitten should die or have to be destroyed due to a genetic or congenital defect, the Seller will replace the Kitten with a Kitten at the Seller’s earliest convenience, should the following conditions be met: the Purchaser provides the Seller with a necropsy by a licensed impartial Veterinary Pathologist, at the Purchaser’s expense, stating that the cause of death is unquestionably a genetic or congenital defect; a second examination by a veterinarian of the Seller’s choice confirms these claims.
+            If, during these 24 months, the Kitten should die or have to be put down due to a genetic or congenital defect, the Seller will replace the Kitten with a Kitten at the Seller’s earliest convenience, should the following conditions be met: the Purchaser provides the Seller with a necropsy by a licensed impartial Veterinary Pathologist, at the Purchaser’s expense, stating that the cause of death is unquestionably a genetic or congenital defect; a second examination by a veterinarian of the Seller’s choice confirms these claims.
           </p>
         </div>
       </div>
