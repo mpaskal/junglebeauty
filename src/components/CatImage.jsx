fbb1572107af82df612a4462ae26c14e5f9e89ee
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-06-12 19:11:21 -0400

    Added prop for images in cat card

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 import { GetImage } from './FirebaseFunctions';
 
-const CatImage = ({ filepath, clickable = true }) => {
+const CatImage = ({ filepath, card = false }) => {
     const [image, setImage] = useState();
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -19,7 +19,7 @@ const CatImage = ({ filepath, clickable = true }) => {
 
     return (
       <>
-        <img className='cat-img' src={image} alt={filepath} onClick={clickable ? handleShow : undefined} />
+        <img className={`cat-img ${card && 'card-image'}`} src={image} alt={filepath} onClick={card ? undefined : handleShow} />
 
         <Modal show={show} onHide={handleClose} size='lg'>
             <Modal.Header closeButton/>
