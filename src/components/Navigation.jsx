8b1c913d99b280b4fe276d904318a134f21427d1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-07-29 22:00:32 -0400

    Made navbar collapse when navigating to new page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ import './../App.css';
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect className='nav-bar' expand="lg" variant='dark' bg='dark'>
+        <Navbar collapseOnSelect className='nav-bar' expand='false' variant='dark' bg='dark'>
             <Container>
                 <Link to='/'>
                     <Navbar.Brand className='logo-link'>
@@ -16,25 +16,25 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
                 <Navbar.Collapse id='responsive-navbar-nav'>
                     <Nav className='ml-auto'>
-                        <NavLink className='nav-link' to='/' end>Home</NavLink>
-                        <NavLink className='nav-link' to='/queens'>Queens</NavLink>
-                        <NavLink className='nav-link' to='/kings'>Kings</NavLink>
-                        <NavLink className='nav-link' to='/kittens'>Kittens</NavLink>
-                        <NavLink className='nav-link' to='/news'>News</NavLink>
-                        <NavLink className='nav-link' to='/faq'>FAQ</NavLink>
-                        <NavLink className='nav-link' to='/about-us'>About</NavLink>
-                        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
+                        <Nav.Link className='nav-link' as={Link} to='/' eventKey='0' end>Home</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/queens' eventKey='1'>Queens</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/kings' eventKey='2'>Kings</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/kittens' eventKey='3'>Kittens</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/news' eventKey='4'>News</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/faq' eventKey='5'>FAQ</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/about-us' eventKey='6'>About</Nav.Link>
+                        <Nav.Link className='nav-link' as={Link} to='/contact' eventKey='7'>Contact</Nav.Link>
                         <NavDropdown className='nav-dropdown' variant='dark' title='How to buy'>
                             <div className='dropdown-links-container'>
-                                <NavLink className='dropdown-link' to='/why-choose-junglebeauty'>Why choose JungleBeauty</NavLink>
+                                <Nav.Link className='dropdown-link' as={Link} to='/why-choose-junglebeauty' eventKey='7'>Why choose JungleBeauty</Nav.Link>
                                 <NavDropdown.Divider/>
-                                <NavLink className='dropdown-link' to='/buy-sell-agreement'>Buy-Sell Agreement</NavLink>
+                                <Nav.Link className='dropdown-link' as={Link} to='/buy-sell-agreement' eventKey='8'>Buy-Sell Agreement</Nav.Link>
                                 <NavDropdown.Divider/>
-                                <NavLink className='dropdown-link' to='/price'>Price</NavLink>
+                                <Nav.Link className='dropdown-link' as={Link} to='/price' eventKey='9'>Price</Nav.Link>
                                 <NavDropdown.Divider/>
-                                <NavLink className='dropdown-link' to='/preparing-for-coming-kitten'>How to be ready for coming kitten</NavLink>
+                                <Nav.Link className='dropdown-link' as={Link} to='/preparing-for-coming-kitten' eventKey='10'>How to be ready for coming kitten</Nav.Link>
                                 <NavDropdown.Divider/>
-                                <NavLink className='dropdown-link' to='/visit-junglebeauty'>How to visit JungleBeauty</NavLink>
+                                <Nav.Link className='dropdown-link' as={Link} to='/visit-junglebeauty' eventKey='11'>How to visit JungleBeauty</Nav.Link>
                             </div>
                         </NavDropdown>
                     </Nav>
