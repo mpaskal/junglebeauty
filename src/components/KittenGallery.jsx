c232c3dc59ca35230a5d477158194b6a036e6f74
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-10 01:26:47 -0400

    Added warnings for invalid selection and selection yielding no cats

--- src/components/KittenGallery.jsx
+++ src/components/KittenGallery.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import { useCats } from '../contexts/CatsContext';
+import { filterKittens } from './Functions';
 import CatCard from './CatCard';
 import FilterAccordion from './FilterAccordion';
 import './../App.css';
@@ -41,21 +42,24 @@ const KittenGallery = () => {
       setFilters({colour: colours, father: parents.filter((cat) => cat.sex === 'male').map((cat) => {return cat.name}), mother: parents.filter((cat) => cat.sex === 'female').map((cat) => {return cat.name}), status: statuses, sex: sexes});
     }
   }
-
-  console.log(filters);
   
   return (
     <>
       <FilterAccordion handleChange={handleChange} toggleAll={toggleAll} filters={filters} options={{colours, statuses, sexes, parents}} />
 
-      <div className='gallery-container'>
-        {kittens.filter((cat) => 
-          filters.colour.includes(cat.colour)
-          && filters.father.includes(cat.father)
-          && filters.mother.includes(cat.mother)
-          && filters.status.includes(cat.status)
-          && filters.sex.includes(cat.sex))
-          .sort((a, b) => a.date > b.date ? -1 : 1)
+      {(filters['status'].length === 0 || filters['colour'].length === 0 || filters['sex'].length === 0 || filters['father'].length === 0 || filters['mother'].length === 0) ?
+        <div className='filter-warning selection-warning'>
+            <h5>Please select at least one status, fur colour, gender, father, and mother to display kittens.</h5>
+        </div>
+        :
+        filterKittens(kittens, filters).length === 0 &&
+          <div className='filter-warning'>
+            <h5>No kittens match the current selection.</h5>
+          </div> 
+      }
+
+      <div className='gallery-container'> 
+        {filterKittens(kittens, filters).sort((a, b) => a.date > b.date ? -1 : 1)
           .map((cat) => {
             return (
               <CatCard key={cat.id} cat={cat} size='small' />
