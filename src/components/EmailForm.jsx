6d8777e62d44c7f8b19ce66691ba6ce64cd46e41
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-10 18:55:31 -0400

    Got environment variables working

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -4,9 +4,9 @@ import { Swal } from 'sweetalert2';
 import { send } from 'emailjs-com';
 import './../App.css';
 
-const SERVICE_ID = process.env.SERVICE_ID;
-const TEMPLATE_ID = process.env.TEMPLATE_ID;
-const USER_ID = process.env.USER_ID;
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_USER_ID;
 
 const EmailForm = () => {
     const [email, setEmail] = useState({to_email: 'junglebeautymailer@gmail.com', from_email: '', from_name: '', message: ''});
@@ -14,6 +14,7 @@ const EmailForm = () => {
     const handleChange = (event) => {
         const { name, value } = event.target;
         setEmail({ ...email, [name]: value });
+        console.log(process.env.REACT_APP_SERVICE_ID)
     }
 
     const handleSend = (e) => {
@@ -25,7 +26,13 @@ const EmailForm = () => {
             USER_ID
             )
         .then(() => {
-            alert(`Thank you for reaching out! We will get back to you as soon as possible.`);
+            // Swal.fire({
+            //     icon: 'success',
+            //     title: 'Thank you for reaching out! We will get back to you as soon as possible.',
+            //     color: '#FAFAFA',
+            //     confirmButtonColor: '#046E1B'
+            // })
+            alert('success')
         })
         .catch((err) => {
             alert(`We're sorry, this email could not be sent.`);
