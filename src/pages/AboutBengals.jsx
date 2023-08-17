96094c3e573bf3a3adc17a07171c78988b5d16fa
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-17 18:15:46 -0400

    Created page for info about Bengals

---
+++ src/pages/AboutBengals.jsx
@@ -0,0 +1,14 @@
+import './../App.css';
+
+const AboutBengals = () => {
+    return (
+      <div className='page-background'>
+        <h1 className='page-header padded-header'>History of the Bengal Breed</h1>
+        <p>
+          
+        </p>
+      </div>
+    );
+  };
+
+export default AboutBengals;
