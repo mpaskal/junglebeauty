3c71f36254e4cb8d2d6db6588a83a85684b5a7ea
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-17 18:15:29 -0400

    Created list of articles

---
+++ src/lists/ArticleList.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+const ArticleList = [
+    {
+        title: `The Best Age to Adopt a Bengal Kitten`,
+        date: '2023-08-15',
+        body:
+            <>
+                
+            </>
+    }
+
+    /*
+    {
+        title: ``,
+        body:
+            <>
+                <p>
+
+                </p>
+            </>
+    }
+    */
+];
+
+export default ArticleList;
