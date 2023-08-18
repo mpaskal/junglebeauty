00187d525d5234b0a42364d7ad67838ca57d8dd1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-17 22:31:36 -0400

    Brought over page on Bengal features

--- src/pages/AboutBengals.jsx
+++ src/pages/AboutBengals.jsx
@@ -2,12 +2,90 @@ import './../App.css';
 
 const AboutBengals = () => {
     return (
-      <div className='page-background'>
-        <h1 className='page-header padded-header'>History of the Bengal Breed</h1>
-        <p>
-          
-        </p>
-      </div>
+        <>
+            <div className='page-background'>
+                <h1 className='page-header padded-header'>Specific Features of Bengal Cats</h1>
+                <p>
+                    The Bengal is a domestic cat breed created by breeding an Asian leopard cat with a domestic cat. The result is a highly intelligent, energetic feline with a striking coat pattern. Here are ten specific features of Bengals that make them a unique and fascinating breed and what we see every day watching our Bengal kittens and cats in our Junglebeauty Cattery:
+                </p>
+                <ul>
+                    <li>
+                        <b>COAT:</b> Bengals have distinctive coats covered in spots, rosettes or marbled patterns in shades of brown, gold, orange, silver and snow that resemble a wild leopard. It gives them an exotic, wild look that will turn heads. The fur of every Bengal kitten is also very short, dense, silky and incredibly soft, making them very pleasant to pet.
+                    </li>
+                    <li>
+                        <b>ATHLETICISM:</b> Bengals are incredibly athletic and love to climb, jump, and play. Every Bengal kitten is an excellent jumper and can reach impressive heights.
+                    </li>
+                    <li>
+                        <b>INTELLIGENCE:</b> Bengals are very intelligent cats that are easy to train. They can learn to do tricks and are very good at problem-solving. They are also very curious and love to explore everything around them.
+                    </li>
+                    <li>
+                        <b>ENERGY:</b> Bengal cats have a lot of energy and require plenty of exercise and stimulation. They are not lap cats and prefer to play and explore.
+                    </li>
+                    <li>
+                        <b>SOCIABILITY:</b> Bengals are social cats and enjoy the company of their human family. They are affectionate and love to be near their humans, follow their owners in many activities and enjoy participating in events. They are also very social and get along well with other cats and dogs.
+                    </li>
+                    <li>
+                        <b>VOCALIZATION:</b> Bengals are talkative cats known for their unique vocalizations. They will communicate with their humans and make a variety of noises.
+                    </li>
+                    <li>
+                        <b>WATER-LOVING:</b> Bengals are unique in that they love water. They often play in the water and may join their humans in the shower or bath.
+                    </li>
+                    <li>
+                        <b>HUNTING INSTINCTS:</b> Bengals have a strong hunting instinct and enjoy playing with toys that mimic prey. They may also try to hunt small animals in the home.
+                    </li>
+                    <li>
+                        <b>HEALTH:</b> Bengals are generally healthy cats with a lifespan of 12-16 years. They have a strong immune system and a robust digestive system.
+                    </li>
+                    <li>
+                        <b>PERSONALITY:</b> Finally, Bengal cats and kittens have unique and playful personality that makes them a joy to be around. They are curious, adventurous, and always up for a good game.
+                    </li>
+                </ul>
+                <h2 className='page-subheader'>Intelligence</h2>
+                <p>Bengals are a highly intelligent breed of cat that are known for their ability to learn quickly and their curious nature. They are very active and playful cats that need plenty of mental stimulation, and their intelligence makes them a joy to be around.</p>
+                <p>Here are a few of the key features of the Bengals' intelligence:</p>
+                <ul>
+                    <li>
+                        Problem-solving: Every Bengal kitten and cat is very good at problem-solving and can figure out how to get what he wants. Bengal cats will often investigate things that interest them and try to find a way to get to them. This makes them very curious and inquisitive cats that are always exploring their environment.
+                    </li>
+                    <li>
+                        Trainability: Bengal kittens are very easy to train and can learn a wide range of tricks and commands. They are very responsive to positive reinforcement and can quickly learn what behaviors are rewarded. This makes them a joy to train and work with.
+                    </li>
+                    <li>
+                        Adaptability: Bengals are very adaptable cats that can quickly adjust to new environments and situations. They are very curious and love to explore their surroundings, which makes them very adaptable to new situations. This makes them great pets for people who travel frequently or have a changing lifestyle.
+                    </li>
+                    <li>
+                        Communication: Bengal cat and kittens are very good communicators and can understand human language and body language. They can also communicate with other cats and animals through vocalizations and body language. This makes them very social and easy to communicate with.
+                    </li>
+                    <li>
+                        Curiosity: Bengals are very curious cats that love to explore their environment. They are always investigating new things and trying to figure out how things work. This makes them very intelligent cats that are always learning and growing.
+                    </li>
+                </ul>
+                <h2 className='page-subheader'>Vocalization</h2>
+                <p>Bengals are known for their vocalization skills and use a wide range of sounds to communicate with their owners and other cats. They are very social cats that enjoy interacting with their owners and other pets, and their vocalization skills make them easy to understand.</p>
+                <p>Here are a few of the key features of the Bengals' vocalization skills:</p>
+                <ul>
+                    <li>
+                        Meowing: Bengal cats and kittens meow to communicate with their owners and other cats. They use different types of meows to express different emotions and needs. For example, a short meow might indicate a greeting, while a long meow might indicate hunger or a desire for attention.
+                    </li>
+                    <li>
+                        Purring: Almost every Bengal kitten or cat purrs when he is happy or relaxed. Purring is a sign of contentment and is often associated with cats that feel safe and loved.
+                    </li>
+                    <li>
+                        Chirping: Bengals also chirp, a unique sound similar to a bird's chirp. This sound is often used to express excitement or curiosity.
+                    </li>
+                    <li>
+                        Growling: Some Bengal cat and kittens growl when they feel threatened or afraid. This is a warning sign that they are not feeling safe and that they may become aggressive if they feel they need to defend themselves.
+                    </li>
+                    <li>
+                        Screaming: Bengals have a unique scream that is very loud and can be heard from a distance. This sound is often used when they are in distress or feeling threatened.
+                    </li>
+                </ul>
+            </div>    
+            <div className='page-background'>
+                <h1 className='page-header padded-header'>History of the Bengal Breed</h1>
+                
+            </div>    
+        </>
     );
   };
 
