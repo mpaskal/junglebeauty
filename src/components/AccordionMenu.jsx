529aab695560b3b80872672462e91bafc297e192
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-22 23:42:16 -0400

    Adjusted presentation of accordion body text

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -13,19 +13,19 @@ const AccordionMenu = ({ items }) => {
           itemID++;
 
           return (
-              <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
-                  <Accordion.Header className='accordion-header'>
-                    <p className='accordion-header-text'>
-                      {item.date && `${convertDate(item.date)}: `} 
-                      {item.title} 
-                      {(item.type === 'litter' && item.date) && ` - ready to go from ${convertDate(getReleaseDate(item.date))}`}
-                    </p>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    {item.body}
-                    {(item.king && item.queen) && <p>King: <Link to='/kings' state={item.king}>{item.king}</Link>, Queen: <Link to='/queens' state={item.queen}>{item.queen}</Link></p>}
-                  </Accordion.Body>
-              </Accordion.Item>
+            <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
+                <Accordion.Header className='accordion-header'>
+                  <p className='accordion-header-text'>
+                    {item.date && `${convertDate(item.date)}: `} 
+                    {item.title} 
+                    {(item.type === 'litter' && item.date) && ` - ready to go from ${convertDate(getReleaseDate(item.date))}`}
+                  </p>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <p>{item.body}</p>
+                  {(item.king && item.queen) && <p>King: <Link to='/kings' state={item.king}>{item.king}</Link>, Queen: <Link to='/queens' state={item.queen}>{item.queen}</Link></p>}
+                </Accordion.Body>
+            </Accordion.Item>
           )
         }
       })}
