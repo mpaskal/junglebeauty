79892e979bed0b74a00873ea768b6d26d947e2c6
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-14 21:52:26 -0400

    Removed if statement

--- src/components/AccordionMenu.jsx
+++ src/components/AccordionMenu.jsx
@@ -9,25 +9,23 @@ const AccordionMenu = ({ items }) => {
   return (
     <Accordion className='accordion' alwaysOpen>
       {items.map((item) => {
-        if (item.title && item.body) {
-          itemID++;
+        itemID++;
 
-          return (
-            <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
-                <Accordion.Header className='accordion-header'>
-                  <p className='accordion-header-text'>
-                    {item.date && `${convertDate(item.date)}: `} 
-                    {item.title} 
-                    {(item.type === 'litter' && item.date) && ` - ready to go from ${convertDate(getReleaseDate(item.date))}`}
-                  </p>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <p>{item.body}</p>
-                  {(item.king && item.queen) && <p>King: <Link to='/kings' state={item.king}>{item.king}</Link>, Queen: <Link to='/queens' state={item.queen}>{item.queen}</Link></p>}
-                </Accordion.Body>
-            </Accordion.Item>
-          )
-        }
+        return (
+          <Accordion.Item className='accordion-item' key={itemID} eventKey={itemID}>
+              <Accordion.Header className='accordion-header'>
+                <p className='accordion-header-text'>
+                  {item.date && `${convertDate(item.date)}: `} 
+                  {item.title && item.title} 
+                  {(item.type === 'litter' && item.date) && ` - ready to go from ${convertDate(getReleaseDate(item.date))}`}
+                </p>
+              </Accordion.Header>
+              <Accordion.Body>
+                <p>{item.body && item.body}</p>
+                {(item.king && item.queen) && <p>King: <Link to='/kings' state={item.king}>{item.king}</Link>, Queen: <Link to='/queens' state={item.queen}>{item.queen}</Link></p>}
+              </Accordion.Body>
+          </Accordion.Item>
+        )
       })}
     </Accordion>
   );
