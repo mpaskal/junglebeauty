e18c7747d0c91422c2852a7dfc6934479a94be42
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-14 21:51:12 -0400

    Edited formatting

--- src/pages/PrepareForKitten.jsx
+++ src/pages/PrepareForKitten.jsx
@@ -9,9 +9,10 @@ const PrepareForKitten = () => {
           <li>
             <Row className='item-container'>
               <Col>
-                You need a carrier for the kitten to bring them home safely and take them to the vet clinic later on.
-                We recommend having a carrier big enough for a full grown cat. It can be a soft carrier or a plastic carrier. We suggest having a double door carrier; here is an example:
-                <p/>
+                <p>
+                  You need a carrier for the kitten to bring them home safely and take them to the vet clinic later on.
+                  We recommend having a carrier big enough for a full grown cat. It can be a soft carrier or a plastic carrier. We suggest having a double door carrier; here is an example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/top-load%20carrier.jpg' alt='Top-load cat carrier'/>
@@ -20,10 +21,12 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                You need a litter box for the kitten. For example:
-                <p/>
-                Note: our kittens are trained to use different litter boxes - from simple tray to open top to enclosed.
-                <p/>
+                <p>
+                  You need a litter box for the kitten. For example:
+                </p>
+                <p>
+                  Note: our kittens are trained to use different litter boxes - from simple tray to open top to enclosed.
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/cat_litter_box.jpg' alt='Litter box'/>
@@ -32,8 +35,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                You need litter for the kitten. For example, Clumping Wood Litter from ökocat:
-                <p/>
+                <p>
+                  You need litter for the kitten. For example, Clumping Wood Litter from ökocat:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/ococat.jpg' alt='Bag of ökocat litter'/>
@@ -42,8 +46,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                You need a scratching post for the kitten. For example:
-                <p/>
+                <p>
+                  You need a scratching post for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/scratchinPost.jpg' alt='Scratching post'/>
@@ -52,8 +57,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                You need a sleeping bed for the kitten. For example:
-                <p/>
+                <p>
+                  You need a sleeping bed for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/sleepingBed.jpg' alt='Cat bed'/>
@@ -62,8 +68,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                You need 2 or more bowls for water. We recommend ceramic or stainless bowls. For example:
-                <p/>
+                <p>
+                  You need 2 or more bowls for water. We recommend ceramic or stainless bowls. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/ceramic bowls for kitten.jpg' alt='Ceramic food and water bowls on a stand'/>
@@ -72,8 +79,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-              Kittens like the Cat Drinking Fountain. Many cats and especially Bengals love running water. That is why they prefer to drink from the tap or fountains. It also encourages them to drink more which is very important for cat health. Just do not forget to refresh the water every 1-2 days. For example:
-                <p/>
+                <p>
+                  Kittens like the Cat Drinking Fountain. Many cats and especially Bengals love running water. That is why they prefer to drink from the tap or fountains. It also encourages them to drink more which is very important for cat health. Just do not forget to refresh the water every 1-2 days. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/cat_water_fountain.jpg' alt='Cat drinking from a water fountain'/>
@@ -82,8 +90,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                Toys for the kitten. For example:
-                <p/>
+                <p>
+                  Toys for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/kitten_toys.jpg' alt='Feather and fuzzy worm toys, wand toys with feathers on strings, and a tube for cats to crawl through'/>
@@ -92,8 +101,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                Laser toy for the kitten. For example:
-                <p/>
+                <p>
+                Laser pointer toy for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/laser_toy.jpg' alt='Laser pointer'/>
@@ -102,8 +112,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                Cat tree for the kitten. For example:
-                <p/>
+                <p>
+                  Cat tree for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/cat_tree.jpg' alt='Cat lying on top of multi-level cat tree'/>
@@ -112,8 +123,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                Heating pad for the kitten. Just recommended. We would recommend buying such a pad with settings for 80-130℉. For example:
-                <p/>
+                <p>
+                  Heating pad for the kitten. Just recommended. We would recommend buying such a pad with settings for 80-130℉. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/heating_pad.jpg' alt='Puppy lying on heating pad'/>
@@ -122,8 +134,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                Brush for the kitten. For example:
-                <p/>
+                <p>
+                  Brush for the kitten. For example:
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/CatBrush.jpg' alt='Cat being brushed'/>
@@ -132,8 +145,9 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
+                <p>
                 Nail care: trim front paw nails every 2-3 weeks. We found toenail clippers for people work best for this purpose. Please buy a high quality item to avoid discomfort and injury. For example:
-                <p/>
+                </p>
               </Col>
               <Col xl={3}>
                 <img className='side-img' src='/assets/site/ready/toenailClipper.jpg' alt='Toenail clipper'/>
@@ -142,50 +156,42 @@ const PrepareForKitten = () => {
           </li><li>
             <Row className='item-container'>
               <Col>
-                The exercise wheel is one of the most popular items for Bengals. They absolutely love it. However, you can buy it later on when the kitten is over one year old. For example:
-                <p/>
+                <p>
+                  The exercise wheel is one of the most popular items for Bengals. They absolutely love it. However, you can buy it later on when the kitten is over one year old. For example:
+                </p>
               </Col>
               <Col xl={3}>
-              <img className='side-img' src='/assets/site/ready/OneFastCat.jpg' alt='Cat running on exercise wheel'/>
+                <img className='side-img' src='/assets/site/ready/OneFastCat.jpg' alt='Cat running on exercise wheel'/>
               </Col>
             </Row>
           </li>
         </ol>
+
         <h2>Food and water</h2>
         <ol className='list-container'>
           <li>
-          <Row className='item-container'>
-            <Col>
+            <p>
               Dry Royal Canin: Mother and Baby Cat and Kitten formulas - will be provided from us at first.
-              <p/>
-            </Col>
-            <Col xl={3}>
-              <img className='side-img' src='/assets/site/ready/royal_canin_mother_baby_cat.jpg' alt='Bag of Royal Canin Mother & Baby Cat dry food'/>
-              <img className='side-img' src='/assets/site/ready/royal_canin_kitten.jpg' alt='Bag of Royal Canin Kitten dry food'/>
-            </Col>
-          </Row>
-          </li><li>
-          <Row className='item-container'>
-            <Col>
+            </p>
+            <br />
+            <img className='side-img' src='/assets/site/ready/royal_canin_mother_baby_cat.jpg' alt='Bag of Royal Canin Mother & Baby Cat dry food'/>
+            <img className='side-img' src='/assets/site/ready/royal_canin_kitten.jpg' alt='Bag of Royal Canin Kitten dry food'/>
+          </li>
+          <li>
+            <p>
               Canned Royal Canin: Mother and Baby Cat and Kitten formulas - samples will be provided by us for the first few days.
-              <p/>
-            </Col>
-            <Col xl={3}>
-              <img className='side-img' src='/assets/site/ready/wet_mother_baby.jpg' alt='Can of Royal Canin Mother & Baby Cat wet food'/>
-              <img className='side-img' src='/assets/site/ready/wet_kitten.jpg' alt='Can of Royal Canin Kitten wet food'/>
-            </Col>
-          </Row>
-          </li><li>
-          <Row className='item-container'>
-            <Col>
+            </p>
+            <br />
+            <img className='side-img' src='/assets/site/ready/wet_mother_baby.jpg' alt='Can of Royal Canin Mother & Baby Cat wet food'/>
+            <img className='side-img' src='/assets/site/ready/wet_kitten.jpg' alt='Can of Royal Canin Kitten wet food'/>
+          </li>
+          <li>
+            <p>
               Our kittens have tried the raw diet which is recommended for Bengal Cats. We will share more details on pick up time. Food samples will be provided. We recommend Big Country Raw with Pure Chicken and IronWillRaw with Basic Chicken.
-              <p/>
-            </Col>
-            <Col xl={3}>
-              <img className='side-img' src='/assets/site/ready/BigCountryRaw_PureChicken.jpg' alt='Box of Big Country Raw Chicken'/>
-              <img className='side-img' src='/assets/site/ready/IronWillRaw.jpg' alt='Box of Iron Will Raw Chicken'/>
-            </Col>
-          </Row>
+            </p>
+            <br />
+            <img className='side-img' src='/assets/site/ready/BigCountryRaw_PureChicken.jpg' alt='Box of Big Country Raw Chicken'/>
+            <img className='side-img' src='/assets/site/ready/IronWillRaw.jpg' alt='Box of Iron Will Raw Chicken'/>
           </li>
         </ol>
         <h2>How our kittens are fed</h2>
